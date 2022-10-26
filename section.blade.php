89859b71d8c75d4214d0d124e06732b8743a6dec
Heocone <116701989+Heocone@users.noreply.github.com>
2022-10-26 12:02:35 +0700

    Add files via upload

---
+++ section.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    {{-- <-- CSS --> --}}
+    <style>
+body p {
+    
+    color:dimgrey;
+    margin-right: 45px;
+    margin-left: 45px;
+    font-style: italic;
+    font-size: 20px;
+    text-align: center;
+
+}
+
+h1 {
+
+    width: 100%;
+    font-size: 75px;
+    text-align: center;
+    color:orange;
+    clear: both;
+}
+
+.hr {
+    width: 100%;
+    height: 20px;
+    background-color: white;
+    clear: both;
+}
+
+.IBM {
+    width: 50%;
+    height: 200px;
+    background-color: none;
+    float: left;
+}
+
+.Microsoft {
+    width: 50%;
+    height: 200px;
+    background-color: none;
+    float: right;
+}
+
+.Iphone {
+    margin-top: 50px;
+    width: 50%;
+    height: 300px;
+    background-color: none;
+    float: left;
+}
+
+.Google {
+    width: 50%;
+    height: 300px;
+    background-color: none;
+    float: right;
+}
+.IBM img {
+    margin-left: 250px;
+    width: 200px;
+    height: 100px;
+}
+
+.Microsoft img {
+    margin-left: 300px;
+    width: 105px;
+    height: 100px;
+}
+
+.Iphone img {
+    margin-bottom: 10px;
+    margin-left: 300px;
+    width: 100px;
+    height: 100px;
+}
+
+.Google img {
+    margin-bottom: 10px;
+    margin-left: 200px;
+    width: 400px;
+    height: 200px;
+    clear: left;
+}
+.Google p{
+    position: relative;
+    bottom:80px;
+}
+.Iphone p{
+    position: relative;
+    bottom:30px;
+}
+.IBM p{
+    position: relative;
+    bottom:20px;
+}
+.Microsoft p{
+    position: relative;
+    bottom:20px;
+}
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello world</title>
+    <h1>What Our Clients Say</h1>
+</div>
+
+<div class="hr">
+</div>
+
+</head>
+<body>
+    <section>
+        <div class="IBM">
+        <img src=" https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg" >
+        <p>Sed vestibulum scelerisque urna, eu finibus leo facilisis sit amet. Proin id dignissim magna. Sed varius urna et pulvinar venenatis.</p>
+        </div>
+
+        <div class="Microsoft">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png"  />
+        <p>Donec euismod dolor ut ultricies consequat. Vivamus urna ipsum, rhoncus molestie neque ac, mollis eleifend nibh.</p>
+        </div>
+
+        <div class="hr">
+        </div>
+
+        <div class="Iphone">
+        <img src="https://media.idownloadblog.com/wp-content/uploads/2018/07/Apple-logo-black-and-white.png">
+        <p>In efficitur in velit et tempus. Duis nec odio dapibus, suscipit erat fringilla, imperdiet nibh. Morbi tempus autor felis ac vehicula.</p>
+        </div>
+
+        <div class="Google">
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/010/930/001/small/google-logo-icon-illustration-free-vector.jpg">
+        <p>Fusce pharetra erat id odio blandit, nec pharetra eros venenatis. Pellentesque porttior cursus massa et vestibulum.</p>
+        </div>
+    </section>
+
+</body>
+</html>
